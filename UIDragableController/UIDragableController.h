e15b27d6d5f073c3ec2bfb6ad0788461cda2a9b5
Rolando Rodriguez <cxavier71@icloud.com>
2018-11-06 18:22:02 -0500

    Initial Commit

---
+++ UIDragableController/UIDragableController.h
@@ -0,0 +1,19 @@
+//
+//  UIDragableController.h
+//  UIDragableController
+//
+//  Created by Rolando Rodriguez on 11/6/18.
+//  Copyright © 2018 RRY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIDragableController.
+FOUNDATION_EXPORT double UIDragableControllerVersionNumber;
+
+//! Project version string for UIDragableController.
+FOUNDATION_EXPORT const unsigned char UIDragableControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIDragableController/PublicHeader.h>
+
+
